f666314f97656dbec1bda2ea8cd7ed4fab7610c8
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-07-23 16:35:14 +0530

    fix : layout fixes

--- lib/widgets/gradient_scaffold.dart
+++ lib/widgets/gradient_scaffold.dart
@@ -1,31 +1,24 @@
 import 'package:flutter/material.dart';
 
-import '../theme/sortjoy_theme.dart';
+import 'sortjoy_background.dart';
 
 class GradientScaffold extends StatelessWidget {
   const GradientScaffold({
     super.key,
     required this.child,
-    this.colors = const [SortJoyColors.skyTop, SortJoyColors.skyBottom],
   });
 
   final Widget child;
-  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: colors,
-          ),
-        ),
-        child: child,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const SortJoyBackground(),
+          child,
+        ],
       ),
     );
   }
