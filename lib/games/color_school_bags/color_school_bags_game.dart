5ef421e32327351f93a33d73d06f609b122581c8
Pranjal Singh <pranjalsingh10061997@gmail.com>
2026-07-24 14:00:49 +0530

    fix : 3d layer to cards

--- lib/games/color_school_bags/color_school_bags_game.dart
+++ lib/games/color_school_bags/color_school_bags_game.dart
@@ -6,7 +6,7 @@ import '../../data/game_catalog.dart';
 import '../../games/sorting_engine/sorting_engine.dart';
 import '../../games/sorting_engine/sorting_play_screen.dart';
 import '../../state/app_state.dart';
-import '../../widgets/sortjoy_background.dart';
+import '../../widgets/app_background.dart';
 
 Future<void> openColorSchoolBagsGame(BuildContext context) async {
   final app = context.read<AppState>();
@@ -50,7 +50,7 @@ Future<void> openColorSchoolBagsGame(BuildContext context) async {
     MaterialPageRoute(
       builder: (_) => SortingPlayScreen(
         config: config,
-        background: const SortJoyBackground(),
+        background: const AppBackground(),
         title: 'Color School Bags',
       ),
     ),
